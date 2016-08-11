c41af1a97d97a21b34065d0a8bee676377c4d523
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-08-10 20:51:11 -0600

    Created variable outlets for text fields, and an action for the save button

--- Prep'r/Prep'r/AddMemberViewController.swift
+++ Prep'r/Prep'r/AddMemberViewController.swift
@@ -10,26 +10,23 @@ import UIKit
 
 class AddMemberViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var bDayTextField: UITextField!
+    
+    @IBOutlet weak var mfoTextField: UITextField!
+    
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    @IBOutlet weak var heightTextField: UITextField!
+    
+    @IBAction func saveMemberButton(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
